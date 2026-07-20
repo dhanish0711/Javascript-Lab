b5b82c2c6c0407e1493e279729e6edd365f21c27
Dhanish Ladwani <dhanishladwani@gmail.com>
2026-07-20 14:41:30 +0530

    Add JS greeting functions for Experiment 1

---
+++ Experiment 1/experiment 1/Experiment 1.js
@@ -0,0 +1,13 @@
+function greet() {
+    alert("Welcome to JavaScript programming!");
+
+    console.table([
+        {
+            Name: "Sample User",
+            Course: "JavaScript"
+        }
+    ]);
+
+    console.time("Execution");
+    console.timeEnd("Execution");
+}
